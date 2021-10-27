0925bab928b7fbaf3eb9614f77f0b687a1741c20
Nuttakit Pipitanasuparat <artsiwat@gmail.com>
2021-10-27 10:16:03 +0700

    initial commit

---
+++ src/main/java/th/co/scb/ncbos/microservices/helloworldrestapi/HelloWorldBean.java
@@ -0,0 +1,12 @@
+package th.co.scb.ncbos.microservices.helloworldrestapi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloWorldBean {
+    private String message;
+}
